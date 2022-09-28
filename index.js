74264d9a3999da6e3c1f3853191be0c79f8f868b
tech-pundit <lord.codefreak@gmail.com>
2022-09-29 00:53:59 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("oop");
+
+
+window.addEventListener("DOMContentLoaded", function load() {
+        button.addEventListener("click", function backgroundParagraphColor() {
+        const paragraph = document.getElementsByTagName("p")[0].classList.toggle("background-color");
+    });
+});
